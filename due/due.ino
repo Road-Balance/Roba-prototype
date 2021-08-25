2c57943f707af9e78b6381565959b709ae34d37d
Jeonghyun Kim <kimdictor@gmail.com>
2021-08-25 17:11:39 +0900

    Implement basic logic which is from teensy

--- due/due.ino
+++ due/due.ino
@@ -1,76 +1,137 @@
-#define ODRIVE_SERIAL Serial1
+#define ODRIVE_FRONT_SERIAL Serial1
+#define ODRIVE_BACK_SERIAL Serial2
+#define ODRIVE_AXIS_STATE_CLOSED_LOOP_CONTROL 8
 
-double d;
-int i;
+#define ODRIVE_COUNT 2
+#define ODRIVE_AXIS_PER_BOARD 2
+#define MOTOR_1 0
+#define MOTOR_2 1
 
-void setup() {
-    Serial.begin(115200);
-    Serial1.begin(115200);
-    Serial.println("init");
-    //Serial1.println("sr");
+int axises[2] = {MOTOR_1, MOTOR_2};
+
+void setup()
+{
+    ODRIVE_FRONT_SERIAL.begin(115200);
+    ODRIVE_BACK_SERIAL.begin(115200);
+    Serial.println("INIT,UARTOK");
+
+    setOdriveClosedLoop(ODRIVE_FRONT_SERIAL);
+    setOdriveClosedLoop(ODRIVE_BACK_SERIAL);
+    Serial.println("INIT,SETSTATEOK");
 }
 
-void loop() {
-    readOdriveVariable("error");
-    if (readInt(&i, 1) > 0) {
-        if (i > 0) {
-            Serial.println(i);
-        } else {
-            Serial.println("no err");
+void loop()
+{
+    byte buf[16];
+    byte len;
+    Serial.setTimeout(5);
+    float axisLeft, axisRight;
+    int i;
+    while (true)
+    {
+        len = Serial.readBytesUntil(0xFF, (uint8_t *)&buf, 16);
+        // buf [1] = a1x, [2] = a1y, [3] = a2x, [4] = a2y
+        if (len > 0)
+        {
+            axisLeft = (buf[2] - 128) * 0.01f;
+            axisRight = (buf[4] - 128) * 0.01f;
+            setOdriveVelocity(ODRIVE_FRONT_SERIAL, MOTOR_1, axisLeft);
+            setOdriveVelocity(ODRIVE_FRONT_SERIAL, MOTOR_2, -axisRight);
+            setOdriveVelocity(ODRIVE_BACK_SERIAL, MOTOR_1, axisLeft);
+            setOdriveVelocity(ODRIVE_BACK_SERIAL, MOTOR_2, -axisRight);
+        }
+
+        if (readInt(ODRIVE_FRONT_SERIAL, &i, 100) > 0)
+        {
+            Serial.println("front err");
+            ODRIVE_FRONT_SERIAL.println("sc");
+            delay(50);
+            setOdriveClosedLoop(ODRIVE_FRONT_SERIAL);
+        }
+        if (readInt(ODRIVE_BACK_SERIAL, &i, 100) > 0)
+        {
+            Serial.println("back err");
+            ODRIVE_BACK_SERIAL.println("sc");
+            delay(50);
+            setOdriveClosedLoop(ODRIVE_BACK_SERIAL);
         }
     }
-    delay(1000);
 }
 
-void setOdriveMode() {
-
+void setOdriveClosedLoop(Stream &port)
+{
+    for (int a = 0; a < ODRIVE_AXIS_PER_BOARD; a++)
+    {
+        setOdriveSetState(port, axises[a], ODRIVE_AXIS_STATE_CLOSED_LOOP_CONTROL);
+    }
 }
 
-void readOdriveVariable(char* varName) {
-    OdriveSerialSprintf("r %s", varName);
+void setOdriveSetState(Stream &port, int axis, int state)
+{
+    OdriveSerialSprintf(port, "w axis%d.requested_state %d", axis, state);
 }
 
-void writeOdriveVariable(char* property, float value) {
-    OdriveSerialSprintf("r %s %f", property, value);
+void setOdriveVelocity(Stream &port, int motorNumber, float velocity)
+{
+    OdriveSerialSprintf(port, "v %d %f %f", motorNumber, velocity, 0.0f);
 }
 
-void writeOdriveVariable(char* property, int value) {
-    OdriveSerialSprintf("r %s %d", property, value);
+void readOdriveVariable(Stream &port, const char *varName)
+{
+    OdriveSerialSprintf(port, "r %s", varName);
 }
 
-void writeOdriveVariable(char* property, char* value) {
-    OdriveSerialSprintf("r %s %s", property, value);
+void writeOdriveVariable(Stream &port, const char *property, float value)
+{
+    OdriveSerialSprintf(port, "r %s %f", property, value);
 }
 
-int readFloat(double* ret, int timeout) {
+void writeOdriveVariable(Stream &port, const char *property, int value)
+{
+    OdriveSerialSprintf(port, "r %s %d", property, value);
+}
+
+void writeOdriveVariable(Stream &port, const char *property, char *value)
+{
+    OdriveSerialSprintf(port, "r %s %s", property, value);
+}
+
+int readFloat(Stream &port, double *ret, int timeout)
+{
     char buf[16];
-    ODRIVE_SERIAL.setTimeout(timeout * 1000);
-    if (ODRIVE_SERIAL.readBytesUntil('\n', buf, 16) <= 0) return -1; 
+    port.setTimeout(timeout * 1000);
+    if (port.readBytesUntil('\n', buf, 16) <= 0)
+        return -1;
     *ret = atof(buf);
     return 1;
 }
 
-int readInt(int* ret, int timeout) {
+int readInt(Stream &port, int *ret, int timeout)
+{
     char buf[16];
-    ODRIVE_SERIAL.setTimeout(timeout * 1000);
-    if (ODRIVE_SERIAL.readBytesUntil('\n', buf, 16) <= 0) return -1; 
+    port.setTimeout(timeout * 1000);
+    if (port.readBytesUntil('\n', buf, 16) <= 0)
+        return -1;
     *ret = atoi(buf);
     return 1;
 }
 
-int8_t readString(char* ret, int timeout) {
+int readString(Stream &port, char *ret, int timeout)
+{
     char buf[16];
-    ODRIVE_SERIAL.setTimeout(timeout * 1000);
-    if (ODRIVE_SERIAL.readBytesUntil('\n', buf, 16) <= 0) return -1; 
+    port.setTimeout(timeout * 1000);
+    if (port.readBytesUntil('\n', buf, 16) <= 0)
+        return -1;
     strcpy(ret, buf);
     return 1;
 }
 
-void OdriveSerialSprintf(char *fmt, ...) {
-  va_list va;
-  va_start(va, fmt);
-  char buf[vsnprintf(NULL, 0, fmt, va) + 1];
-  vsprintf(buf, fmt, va);
-  ODRIVE_SERIAL.println(buf);
-  va_end(va);
+void OdriveSerialSprintf(Stream &port, const char *fmt, ...)
+{
+    va_list va;
+    va_start(va, fmt);
+    char buf[vsnprintf(NULL, 0, fmt, va) + 1];
+    vsprintf(buf, fmt, va);
+    port.println(buf);
+    va_end(va);
 }
