4c76b94e5cc258d475c62be01778cb2a54ecd08e
kimsooyoung <tge1375@naver.com>
2021-10-04 20:19:29 +0900

    Update : new firmware 10/04

--- due/YYH/YYH_1Bytes/YYH_1Bytes.ino
+++ due/YYH/YYH_1Bytes/YYH_1Bytes.ino
@@ -41,20 +41,32 @@ void loop() {
   // 추후 axisLeftY +- p*axisRightX의 값이 axisLeftY의 부호를 그대로 따라가야
   // 함. 1/2.55 = 0.38...
   // float p = 1;
-  float p = 0.38f * axisLeftY;
+  // float p = 0.38f * axisLeftY;
   float FL, RL, FR, RR;
+  float THR, STR,
+      k
 
-  Serial.println("INIT,ENTERLOOP");
+          Serial.println("INIT,ENTERLOOP");
   while (true) {
     // Serial.println("LOOP");
     len = Serial.readBytesUntil(0xFF, (uint8_t *)&buf, 16);
     // buf [1, 2] = a1x, [3, 4] = a1y, [5, 6] = a2x, [7, 8] = a2y
     if (len > 0) {
-      raw_a1y = buf[2] - 128;
-      raw_a2x = buf[3] - 128;
-      axisLeftY = (raw_a1y)*0.02f;
-      axisRightX = (raw_a2x)*0.02f;
-      //사용안함 axisRightY = ((buf[7] * 255 + buf[8]) - 32767) * 0.0001f;
+      raw_a1y = buf[2] - 127; //버퍼엔 127, 152, 177.. 25단위로 들어옴
+      raw_a2x = buf[3] - 127; //버퍼엔 127, 137, 147.. 10단위로 들어옴
+      // -127 ~ +127
+      axisLeftY = (raw_a1y)*0.02f;  // axisLeftY : 0.5, 1.0, 1.5 ...
+      axisRightX = (raw_a2x)*0.01f; // axisRightX : 0.1, 0.2, 0.3 ...
+
+      // THR : Throttle, 전후진 값, 각속도
+      THR = axisLeftY;
+      //최소 반경을 위한 모터 각속도 비 k
+      k = -0.2f * abs(axisLeftY) + 0.6f; // ex) axisLeftY : 1.0 -> k = 0.4
+      // STR : Steering, 조향값. STR은 THR보다 반드시 작음
+      // 1/1.27 = 0.7874
+      STR =
+          THR * (1 - k) * axisRightX * 0.7874f; // axisRightX : 0.1, 0.2, 0.3...
+
       Serial.print(raw_a1y);
       Serial.print(" ");
       Serial.println(raw_a2x);
@@ -65,32 +77,21 @@ void loop() {
 
       Serial.println();
 
-      if (axisLeftY > 0.02f) //전진
-      {
-        Serial.println("frontOK");
-        //여기서 axisLeftY뒤에 세미콜론이 찍혀있었다..
-        FR = axisLeftY - p * axisRightX; //우측 전륜
-        RR = axisLeftY - p * axisRightX; //우측 후륜
-        FL = axisLeftY + p * axisRightX; //좌측 전륜
-        RL = axisLeftY + p * axisRightX; //좌측 후륜
+      //전후진 && 조향, 추후 axis제어시 데드존 필요
+      if (THR != 0.0f) {
+        FL = THR + STR;
+        RL = THR + STR;
+        FR = THR - STR;
+        RR = THR - STR;
       }
-
-      else if (axisLeftY < -0.02f) //후진
-      {
-        Serial.println("RearOK");
-        FR = axisLeftY - p * axisRightX;
-        RR = axisLeftY - p * axisRightX;
-        FL = axisLeftY + p * axisRightX;
-        RL = axisLeftY + p * axisRightX;
-      }
-
-      else //탱크턴
-      {
-        FR = axisRightX;
-        RR = axisRightX;
-        FL = -axisRightX;
-        RL = -axisRightX;
-      }
+      //탱크턴
+      else
+        () {
+          FL = axisRightX * 2.0f;
+          RL = axisRightX * 2.0f;
+          FR = -axisRightX * 2.0f;
+          RR = -axisRightX * 2.0f;
+        }
 
       Serial.print(FR);
       Serial.print(" ");
