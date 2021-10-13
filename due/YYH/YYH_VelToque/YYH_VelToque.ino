3dcaaf3689b04803a3358c15d2f029a21f65546f
kimsooyoung <tge1375@naver.com>
2021-10-13 22:42:23 +0900

    Feature : velocity torque_ff

---
+++ due/YYH/YYH_VelToque/YYH_VelToque.ino
@@ -0,0 +1,191 @@
+#define CONTROL_SERIAL Serial
+#define ODRIVE_FRONT_SERIAL Serial1
+#define ODRIVE_BACK_SERIAL Serial2
+#define ODRIVE_AXIS_STATE_CLOSED_LOOP_CONTROL 8
+
+#define ODRIVE_COUNT 2
+#define ODRIVE_AXIS_PER_BOARD 2
+#define MOTOR_1 0
+#define MOTOR_2 1
+
+#define SERIAL_DELAY 2
+
+int axises[2] = {MOTOR_1, MOTOR_2};
+
+void setup() {
+  ODRIVE_FRONT_SERIAL.begin(115200);
+  ODRIVE_BACK_SERIAL.begin(115200);
+  Serial.begin(115200);
+  Serial.println("INIT,UARTOK");
+
+  OdriveSerialSprintf(ODRIVE_FRONT_SERIAL, "sr");
+  OdriveSerialSprintf(ODRIVE_BACK_SERIAL, "sr");
+  delay(2000);
+  Serial.println("INIT,REBOOTOK");
+
+  setOdriveClosedLoop(ODRIVE_FRONT_SERIAL);
+  setOdriveClosedLoop(ODRIVE_BACK_SERIAL);
+  Serial.println("INIT,SETSTATEOK");
+  delay(2000);
+}
+
+void loop() {
+  byte buf[16];
+  byte len;
+  Serial.setTimeout(5);
+  float axisLeftY, axisRightY, axisRightX;
+  float offsetLeft, offsetRight;
+  int raw_a1y, raw_a2x;
+  double vbusf, vbusb;
+
+  // 추후 axisLeftY +- p*axisRightX의 값이 axisLeftY의 부호를 그대로 따라가야
+  // 함. 1/2.55 = 0.38...
+  // float p = 1;
+  // float p = 0.38f * axisLeftY;
+  float TQ_FORWARD, TQ_BACKWARD;
+  float FL, RL, FR, RR;
+  float THR, STR, k;
+
+  Serial.println("INIT,ENTERLOOP");
+  while (true) {
+    // Serial.println("LOOP");
+    len = Serial.readBytesUntil(0xFF, (uint8_t *)&buf, 16);
+    // buf [1, 2] = a1x, [3, 4] = a1y, [5, 6] = a2x, [7, 8] = a2y
+    if (len > 0) {
+      raw_a1y = buf[2] - 127; //버퍼엔 127, 152, 177.. 25단위로 들어옴
+      raw_a2x = buf[3] - 127; //버퍼엔 127, 137, 147.. 10단위로 들어옴
+
+      // -127 ~ +127
+      axisLeftY = (raw_a1y)*0.02f;  // axisLeftY : 0.5, 1.0, 1.5 ...
+      axisRightX = (raw_a2x)*0.01f; // axisRightX : 0.1, 0.2, 0.3 ...
+
+      // THR : Throttle, 전후진 값, 각속도
+      THR = axisLeftY;
+      //최소 반경을 위한 모터 각속도 비 k
+      k = -0.2f * abs(axisLeftY) + 0.6f; // ex) axisLeftY : 1.0 -> k = 0.4
+      // STR : Steering, 조향값. STR은 THR보다 반드시 작음
+      // 1/1.27 = 0.7874
+      STR =
+          THR * (1 - k) * axisRightX * 0.7874f; // axisRightX : 0.1, 0.2, 0.3...
+
+      // Serial.print(raw_a1y);
+      // Serial.print(" ");
+      // Serial.println(raw_a2x);
+
+      // Serial.print(axisLeftY);
+      // Serial.print(" ");
+      // Serial.println(axisRightX);
+
+      // Serial.println();
+
+      //전후진 && 조향, 추후 axis제어시 데드존 필요
+      if (THR != 0.0f) {
+        FL = THR + STR;
+        RL = THR + STR;
+        FR = THR - STR;
+        RR = THR - STR;
+      }
+      //탱크턴
+      else {
+        FL = axisRightX * 2.0f;
+        RL = axisRightX * 2.0f;
+        FR = -axisRightX * 2.0f;
+        RR = -axisRightX * 2.0f;
+      }
+      TQ_FORWARD = buf[5] / 10;
+      TQ_BACKWARD = buf[6] / 10;
+
+      Serial.print(FR);
+      Serial.print(" ");
+      Serial.print(RR);
+      Serial.print(" ");
+      Serial.print(FL);
+      Serial.print(" ");
+      Serial.print(RL);
+      Serial.print(" / ");
+      Serial.print(TQ_FORWARD);
+      Serial.print(" ");
+      Serial.print(TQ_BACKWARD);
+      Serial.print(" ");
+      Serial.println();
+
+      setOdriveVelocity(ODRIVE_FRONT_SERIAL, MOTOR_1, FR, );
+      // delay(SERIAL_DELAY);
+      setOdriveVelocity(ODRIVE_FRONT_SERIAL, MOTOR_2, -FL, );
+      // delay(SERIAL_DELAY);
+      setOdriveVelocity(ODRIVE_BACK_SERIAL, MOTOR_1, RR);
+      // delay(SERIAL_DELAY);
+      setOdriveVelocity(ODRIVE_BACK_SERIAL, MOTOR_2, -RL);
+      // delay(SERIAL_DELAY);
+    }
+  }
+}
+
+void setOdriveClosedLoop(Stream &port) {
+  for (int a = 0; a < ODRIVE_AXIS_PER_BOARD; a++) {
+    setOdriveSetState(port, axises[a], ODRIVE_AXIS_STATE_CLOSED_LOOP_CONTROL);
+    delay(SERIAL_DELAY);
+  }
+}
+
+void setOdriveSetState(Stream &port, int axis, int state) {
+  OdriveSerialSprintf(port, "w axis%d.requested_state %d", axis, state);
+}
+
+void setOdriveVelocity(Stream &port, int motorNumber, float velocity,
+                       float torque_ff) {
+  OdriveSerialSprintf(port, "v %d %f %f", motorNumber, velocity, torque_ff);
+}
+
+void readOdriveVariable(Stream &port, const char *varName) {
+  OdriveSerialSprintf(port, "r %s", varName);
+}
+
+void writeOdriveVariable(Stream &port, const char *property, float value) {
+  OdriveSerialSprintf(port, "r %s %f", property, value);
+}
+
+void writeOdriveVariable(Stream &port, const char *property, int value) {
+  OdriveSerialSprintf(port, "r %s %d", property, value);
+}
+
+void writeOdriveVariable(Stream &port, const char *property, char *value) {
+  OdriveSerialSprintf(port, "r %s %s", property, value);
+}
+
+int readFloat(Stream &port, double *ret, int timeout) {
+  char buf[16];
+  port.setTimeout(timeout);
+  if (port.readBytesUntil('\n', buf, 16) <= 0)
+    return -1;
+  *ret = atof(buf);
+  return 1;
+}
+
+int readInt(Stream &port, int *ret, int timeout) {
+  char buf[16];
+  port.setTimeout(timeout);
+  if (port.readBytesUntil('\n', buf, 16) <= 0)
+    return -1;
+  *ret = atoi(buf);
+  return 1;
+}
+
+int readString(Stream &port, char *ret, int timeout) {
+  char buf[16];
+  port.setTimeout(timeout);
+  if (port.readBytesUntil('\n', buf, 16) <= 0)
+    return -1;
+  strcpy(ret, buf);
+  return 1;
+}
+
+void OdriveSerialSprintf(Stream &port, const char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  char buf[vsnprintf(NULL, 0, fmt, va) + 1];
+  vsprintf(buf, fmt, va);
+  port.println(buf);
+  port.flush();
+  va_end(va);
+}
---
+++ jetson/python/JoyStick/RBJoyVersion/read_torque_joystick.py
@@ -0,0 +1,141 @@
+import inputs
+import serial
+import asyncio
+
+
+port_name="/dev/ttyACM1"
+baud_rate=115200
+pub_period=0.5
+
+class JoySerialSenderTwoBytes(object):
+
+    # 0 ~ 5 / 10 steps
+    # 0, 0.5, 1.0, 1.5 ...
+    joyDict = {
+        'ABS_X': 127,
+        'ABS_Y': 127,
+        'ABS_RX': 127,
+        'ABS_RY': 127,
+    }
+
+    prevjoyDict = {
+        'ABS_X': 127,
+        'ABS_Y': 127,
+        'ABS_RX': 127,
+        'ABS_RY': 127,
+    }
+
+    btnDict = {
+        'ABS_HAT0X': 0,
+        'ABS_HAT0Y': 0,
+    }
+
+    btnDictPrev = {
+        'ABS_HAT0X': 0,
+        'ABS_HAT0Y': 0,
+    }
+
+    STEP = 10
+
+    def __init__(self):
+        super().__init__()
+
+        print(inputs.devices.gamepads)
+
+        pads = inputs.devices.gamepads
+        if len(pads) == 0:
+            raise Exception("Couldn't find any Gamepads!")
+    
+        # self._ser = serial.Serial(port_name, baud_rate, timeout=10)
+        # if self._ser.name != port_name:
+        #     raise Exception("Couldn't find MCU!")
+
+        self.msg_header = bytes([255, 1])
+        self._loop = asyncio.get_event_loop()
+
+    def parseJoyDict(self):
+
+        output = bytes()
+        bytes_list = []
+
+        for val in self.joyDict.values():
+            # int_val = min( int(val / 257), 254 )
+            # bytes_list.append(int_val)
+            bytes_list.append(val)
+
+        for val in self.btnDict.values():
+            bytes_list.append(val)
+
+        print(bytes_list)
+        output = bytes(bytes_list)
+
+        return output
+
+    def send_signal(self):
+
+        payload = self.parseJoyDict()
+        send_msg = self.msg_header + payload
+        # self._ser.write(send_msg)
+        # print("======================")
+        print(send_msg)
+
+    def joyLoop(self):
+        events = inputs.get_gamepad()
+        for event in events:
+            if (event.code in self.joyDict) or (event.code in self.btnDict):
+                if event.code == "ABS_Y" or event.code == "ABS_RY":
+                    event.state = - event.state + 255 - 1
+                elif event.code in self.btnDict:
+                    if event.state != 0:
+                        if event.code == "ABS_HAT0Y":
+                            self.btnDict[event.code] -= event.state * 5
+                        elif event.code == "ABS_HAT0X":
+                            self.btnDict[event.code] += event.state * 5 
+                        if self.btnDict[event.code] < 0:
+                            self.btnDict[event.code] = 0
+                    
+                    self.send_signal()
+                    print(self.btnDict)
+                    continue
+
+                event.state = min(int((event.state + 32768) / 256), 254) 
+
+                if abs(self.prevjoyDict[event.code] - event.state) > self.STEP:
+                    self.joyDict[event.code] = event.state
+                    self.prevjoyDict[event.code] = event.state
+
+                    self.send_signal()
+                else:
+                    continue
+                
+            elif event.code == 'BTN_NORTH':
+                for key in self.joyDict.keys():
+                    self.joyDict[key] = 128
+                self.btnDict["ABS_HAT0X"] = 0
+                self.btnDict["ABS_HAT0Y"] = 0
+                self.send_signal()
+
+
+
+    async def joyLoopExecutor(self):
+        while True:
+            await self._loop.run_in_executor(None, self.joyLoop)
+
+    def run(self):
+        try:
+            asyncio.ensure_future(self.joyLoopExecutor())
+            self._loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self._loop.close()
+            print("====== Loop Closed =========")
+
+
+if __name__=="__main__":
+
+    my_serial = JoySerialSenderTwoBytes()
+
+    # while True:
+    #     my_serial.joyLoop()
+    my_serial.run()
